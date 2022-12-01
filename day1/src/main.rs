28f48671dac0774ed2611a9ad27daff31752fb20
Nikhil Thomas <nikhil@nthomas.org>
2022-12-01 10:47:38 -0500

    day 1 remove idx

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,13 +3,9 @@ use std::io::prelude::*;
 use std::io::BufReader;
 
 #[derive(Copy, Clone, Default)]
-struct ElfEntry {
-    pub idx: usize,
-    pub calories: usize,
-}
+struct ElfEntry(usize);
 
 fn main() -> std::io::Result<()> {
-    let mut idx = 0;
     let mut running_calories: Option<usize> = None;
     let mut max_elf = ElfEntry::default();
     let mut elf_list: Vec<ElfEntry> = vec![];
@@ -19,14 +15,10 @@ fn main() -> std::io::Result<()> {
         for line in lines {
             let line = line?;
             if line.is_empty() {
-                let new_entry = ElfEntry {
-                    idx: idx,
-                    calories: running_calories.unwrap_or(0),
-                };
-                if new_entry.calories > max_elf.calories {
+                let new_entry = ElfEntry(running_calories.unwrap_or(0));
+                if new_entry.0 > max_elf.0 {
                     max_elf = new_entry;
                 }
-                idx += 1;
                 running_calories = None;
                 elf_list.push(new_entry);
             } else {
@@ -49,26 +41,20 @@ fn main() -> std::io::Result<()> {
 
     // if we hit EOF before newline, we need to flush again
     if running_calories.is_some() {
-        let new_entry = ElfEntry {
-            idx: idx,
-            calories: running_calories.unwrap(),
-        };
-        if new_entry.calories > max_elf.calories {
+        let new_entry = ElfEntry(running_calories.unwrap());
+        if new_entry.0 > max_elf.0 {
             max_elf = new_entry;
         }
         elf_list.push(new_entry);
-        idx += 1;
     }
 
-    println!(
-        "Max calories are {} at idx {}",
-        max_elf.calories, max_elf.idx
-    );
-    elf_list.sort_by(|a, b| b.calories.cmp(&a.calories));
+    println!("Max calories are {}", max_elf.0);
+
+    elf_list.sort_by(|a, b| b.0.cmp(&a.0));
     let sum_of_three = elf_list
         .iter()
         .take(3)
-        .map(|elf| elf.calories)
+        .map(|elf| elf.0)
         .reduce(|acc, cal| acc + cal)
         .unwrap_or(0);
     println!("The sum of the highest three calories is {}", sum_of_three);
