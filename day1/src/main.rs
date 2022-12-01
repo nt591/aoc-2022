48bca5e56ffd1775de58825488209e306a620784
Nikhil Thomas <nikhil@nthomas.org>
2022-12-01 11:27:45 -0500

    day 1 clean up

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,12 +2,11 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-#[derive(Copy, Clone, Default)]
+#[derive(Copy, Clone)]
 struct ElfEntry(usize);
 
 fn main() -> std::io::Result<()> {
     let mut running_calories: Option<usize> = None;
-    let mut max_elf = ElfEntry::default();
     let mut elf_list: Vec<ElfEntry> = vec![];
 
     // assume valid input for now
@@ -15,24 +14,14 @@ fn main() -> std::io::Result<()> {
         for line in lines {
             let line = line?;
             if line.is_empty() {
-                let new_entry = ElfEntry(running_calories.unwrap_or(0));
-                if new_entry.0 > max_elf.0 {
-                    max_elf = new_entry;
+                if running_calories.is_none() {
+                    continue;
                 }
+                let new_entry = ElfEntry(running_calories.unwrap());
                 running_calories = None;
                 elf_list.push(new_entry);
             } else {
                 let parsed_line = line.parse::<usize>();
-                if parsed_line.is_err() {
-                    println!(
-                        "Got error for parsed line {} and got {:?}",
-                        line, parsed_line
-                    );
-                    return Err(std::io::Error::new(
-                        std::io::ErrorKind::Other,
-                        "Invalid err",
-                    ));
-                }
                 let updated = running_calories.unwrap_or(0) + parsed_line.unwrap();
                 running_calories = Some(updated);
             }
@@ -42,15 +31,12 @@ fn main() -> std::io::Result<()> {
     // if we hit EOF before newline, we need to flush again
     if running_calories.is_some() {
         let new_entry = ElfEntry(running_calories.unwrap());
-        if new_entry.0 > max_elf.0 {
-            max_elf = new_entry;
-        }
         elf_list.push(new_entry);
     }
 
-    println!("Max calories are {}", max_elf.0);
-
     elf_list.sort_by(|a, b| b.0.cmp(&a.0));
+    println!("Max calories are {}", elf_list[0].0);
+
     let sum_of_three = elf_list
         .iter()
         .take(3)
