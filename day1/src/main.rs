f171e357d67f73bde0eea6896983b732861311f7
Nikhil Thomas <nikhil@nthomas.org>
2022-12-01 10:43:08 -0500

    day1 formatted

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,18 +1,18 @@
+use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::fs::File;
 
-#[derive(Copy, Clone,Default)]
+#[derive(Copy, Clone, Default)]
 struct ElfEntry {
     pub idx: usize,
     pub calories: usize,
 }
 
-fn main() -> std::io::Result<()>{
+fn main() -> std::io::Result<()> {
     let mut idx = 0;
-    let mut running_calories : Option<usize> = None;
+    let mut running_calories: Option<usize> = None;
     let mut max_elf = ElfEntry::default();
-    let mut elf_list : Vec<ElfEntry> = vec![];
+    let mut elf_list: Vec<ElfEntry> = vec![];
 
     // assume valid input for now
     if let Ok(lines) = read_lines("./input.txt") {
@@ -32,8 +32,14 @@ fn main() -> std::io::Result<()>{
             } else {
                 let parsed_line = line.parse::<usize>();
                 if parsed_line.is_err() {
-                    println!("Got error for parsed line {} and got {:?}", line, parsed_line);
-                    return Err(std::io::Error::new(std::io::ErrorKind::Other, "Invalid err"));
+                    println!(
+                        "Got error for parsed line {} and got {:?}",
+                        line, parsed_line
+                    );
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        "Invalid err",
+                    ));
                 }
                 let updated = running_calories.unwrap_or(0) + parsed_line.unwrap();
                 running_calories = Some(updated);
@@ -54,16 +60,26 @@ fn main() -> std::io::Result<()>{
         idx += 1;
     }
 
-    println!("Max calories are {} at idx {}", max_elf.calories, max_elf.idx);
+    println!(
+        "Max calories are {} at idx {}",
+        max_elf.calories, max_elf.idx
+    );
     elf_list.sort_by(|a, b| b.calories.cmp(&a.calories));
-    let sum_of_three = elf_list.iter().take(3).map(|elf| elf.calories).reduce(|acc, cal| acc + cal).unwrap_or(0);
+    let sum_of_three = elf_list
+        .iter()
+        .take(3)
+        .map(|elf| elf.calories)
+        .reduce(|acc, cal| acc + cal)
+        .unwrap_or(0);
     println!("The sum of the highest three calories is {}", sum_of_three);
 
     Ok(())
 }
 
 fn read_lines<P>(filename: P) -> std::io::Result<std::io::Lines<BufReader<File>>>
-where P: AsRef<std::path::Path>, {
+where
+    P: AsRef<std::path::Path>,
+{
     let file = File::open(filename)?;
     Ok(BufReader::new(file).lines())
 }
