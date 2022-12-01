c36bdf2daa0b4e4345a2bd7e3c24f5a668a9fff4
Nikhil Thomas <nikhil@nthomas.org>
2022-12-01 17:18:49 -0500

    day 1 with binary heap kinda

--- src/day1.rs
+++ src/day1.rs
@@ -1,8 +1,10 @@
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, Eq)]
 struct ElfEntry(usize);
 
 impl From<ElfEntry> for usize {
@@ -11,9 +13,27 @@ impl From<ElfEntry> for usize {
     }
 }
 
+impl PartialOrd for ElfEntry {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for ElfEntry {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+
+impl PartialEq for ElfEntry {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
 pub fn run() -> std::io::Result<()> {
     let mut running_calories: Option<usize> = None;
-    let mut elf_list: Vec<ElfEntry> = vec![];
+    let mut elf_list: BinaryHeap<ElfEntry> = BinaryHeap::new();
 
     // assume valid input for now
     for line in read_lines("./data/day1.txt")? {
@@ -24,7 +44,7 @@ pub fn run() -> std::io::Result<()> {
             }
             let new_entry = ElfEntry(running_calories.unwrap());
             running_calories = None;
-            elf_list.push(new_entry);
+            push_into_heap_with_fixed_size(&mut elf_list, new_entry);
         } else {
             let parsed_line = line.parse::<usize>();
             let updated = running_calories.unwrap_or(0) + parsed_line.unwrap();
@@ -35,16 +55,14 @@ pub fn run() -> std::io::Result<()> {
     // if we hit EOF before newline, we need to flush again
     if running_calories.is_some() {
         let new_entry = ElfEntry(running_calories.unwrap());
-        elf_list.push(new_entry);
+        push_into_heap_with_fixed_size(&mut elf_list, new_entry);
     }
 
-    elf_list.sort_by(|a, b| b.0.cmp(&a.0));
-    let max_cals = elf_list.get(0).map_or(0, |x| (*x).into());
+    let max_cals: usize = elf_list.peek().map_or(0, |x| (*x).into());
     println!("Max calories are {}", max_cals);
 
     let sum_of_three = elf_list
         .into_iter()
-        .take(3)
         .map(|x| x.into())
         .reduce(|acc, cal| acc + cal)
         .unwrap_or(0);
@@ -53,6 +71,24 @@ pub fn run() -> std::io::Result<()> {
     Ok(())
 }
 
+fn push_into_heap_with_fixed_size<T>(heap: &mut BinaryHeap<T>, elem: T) -> ()
+where
+    T: Ord + std::fmt::Debug,
+{
+    // always push into heap, and if heap is too big then flush last out
+    // todo: drain_sorted()
+    heap.push(elem);
+    if heap.len() > 3 {
+        let mut tmp = vec![];
+        for _ in 0..3 {
+            tmp.push(heap.pop().unwrap());
+        }
+        heap.clear();
+        for elem in tmp {
+            heap.push(elem);
+        }
+    }
+}
 fn read_lines<P>(filename: P) -> std::io::Result<std::io::Lines<BufReader<File>>>
 where
     P: AsRef<std::path::Path>,
