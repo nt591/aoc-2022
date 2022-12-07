60d4a652c1a3453c447bc65b2e9584fe0c262aae
Nikhil Thomas <nikhil@nthomas.org>
2022-12-07 14:26:32 -0500

    day 7 with refcells

--- src/day7.rs
+++ src/day7.rs
@@ -1,26 +1,33 @@
+use std::borrow::BorrowMut;
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::rc::Weak;
+
 #[derive(Clone, Debug, Default)]
 struct DirectoryFolder<'a> {
     name: &'a str,
-    directories: Vec<DirectoryFolder<'a>>,
-    files: Vec<usize>, // do we need names?
+    parent: Option<Weak<DirectoryFolder<'a>>>,
+    directories: RefCell<Vec<DirectoryFolder<'a>>>,
+    files: RefCell<Vec<usize>>, // do we need names?
 }
 
 impl<'a> DirectoryFolder<'a> {
     fn directory_size(&self) -> usize {
-        self.files.iter().sum::<usize>()
+        self.files.borrow().iter().sum::<usize>()
             + self
                 .directories
+                .borrow()
                 .iter()
                 .map(|d| d.directory_size())
                 .sum::<usize>()
     }
 
     fn add_dir(&mut self, dir: DirectoryFolder<'a>) {
-        self.directories.push(dir)
+        self.directories.borrow_mut().push(dir)
     }
 
     fn add_file(&mut self, file: usize) {
-        self.files.push(file);
+        self.files.borrow_mut().push(file);
     }
 }
 
@@ -38,20 +45,23 @@ pub fn run() -> anyhow::Result<()> {
 #[cfg(test)]
 mod tests {
     use crate::day7::DirectoryFolder;
+    use std::borrow::BorrowMut;
+    use std::rc::Rc;
     #[test]
     fn it_sums_directories() {
-        let mut root = DirectoryFolder {
+        let root = Rc::new(DirectoryFolder {
             name: "Root",
             ..Default::default()
-        };
-        let mut inner = DirectoryFolder {
+        });
+        let inner = DirectoryFolder {
             name: "Inner",
+            parent: Some(Rc::downgrade(&root)),
             ..Default::default()
         };
-        root.add_file(100);
-        root.add_file(200);
-        inner.add_file(600);
-        root.add_dir(inner);
+        root.files.borrow_mut().push(100);
+        root.files.borrow_mut().push(200);
+        inner.files.borrow_mut().push(600);
+        root.directories.borrow_mut().push(inner);
 
         assert_eq!(900, root.directory_size());
     }
